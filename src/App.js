acf61e97e6eb62ea0b2ae2d04a78a9bb7882f9c6
Max <max@maxinnes.co>
2022-10-01 17:55:03 +0100

    Simple template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-function App() {
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+export default function App() {
   return (
-    <h1>Sample test</h1>
+    <Container>
+      <Row className="justify-content-md-center">
+
+        <Col xs={5}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item style={{textAlign:"center"}}>
+              <a rel="noreferrer" href="https://bitwarden.jimapps.tools" target="_blank">Bitwarden</a>
+            </ListGroup.Item>
+            <ListGroup.Item style={{textAlign:"center"}}>
+              More Soon
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+      </Row>
+    </Container>
   );
 }
-
-export default App;
